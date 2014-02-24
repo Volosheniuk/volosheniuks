de7623d57bd052349262adfbaba3b30fd17ab778
Volosheniuk Serhiy <Onspeeds@bigmir.net>
2014-02-24 19:19:35 +0400

    123

---
+++ cw/task_2.s
@@ -0,0 +1,27 @@
+.data
+	int_label1:
+	.int	3
+	int_label2:
+	.int	4
+	int_label3:
+	.int   5
+
+	format_string:
+	.string	"Hello world %d %d %d\n"
+.text
+	.globl	main
+main:
+	pushl		%ebp
+	movl		%esp, %ebp
+
+	pushl		int_label3
+	pushl   	int_label2
+	pushl   	int_label1
+
+	pushl		$format_string
+	call		printf
+	addl		$8, %esp
+
+	movl		%ebp, %esp
+	popl		%ebp
+	ret
