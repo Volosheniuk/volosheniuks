95af611ae7c0395da5fa4ce9aeac848b6df8c57f
Volosheniuk Serhiy <Onspeeds@bigmir.net>
2014-05-18 02:23:28 +0400

    problem 7 have been solved

--- hw-2/task_7.s
+++ hw-2/task_7.s
@@ -3,13 +3,19 @@
 mytext:
 	.space 100
 mytextcop:
-	.space 100
+	.space 200
 cop:
 	.space 1
+cop2:
+	.space 100
 lengthS:
 	.space 4
 strstr:
 	.string "YOURS STRING:"
+strstr2 :
+	.string "String after rule2:"
+strstr3:
+	.string "string after rule1:"
 
 ennd:
 	.string "\n"
@@ -121,6 +127,131 @@ myloop2:
 ret
 
 
+
+
+
+
+//function 3 letsrule2
+
+letsrule2:
+//prolog
+	pushl %ebp
+  	movl %esp,%ebp
+//main part
+	movl 8(%ebp),%esi
+	movl $mytextcop,%edi
+	movl lengthS,%ecx
+loopf3:
+ 	movl %ecx, ptr	
+	lodsb
+	stosb
+	stosb
+	movl ptr,%ecx
+	loop loopf3
+
+//wright answer
+#printstring
+	pushl $strstr2
+	call printf
+	addl $4,%esp
+	movl lengthS,%ecx
+	addl %ecx,%ecx
+	movl $mytextcop,%esi
+typing2:
+	movl %ecx, count
+	lodsb
+	movl %esi, ptr
+	movl %eax, a
+	pushl a
+	pushl $fmt_str
+	call printf
+	addl $8, %esp
+	movl ptr, %esi
+	movl count, %ecx
+	loop typing2
+
+	pushl $ennd
+	call printf
+	addl $4,%esp
+
+#end of printstring
+//epilog
+	movl %ebp,%esp
+	popl %ebp
+ret
+
+
+
+
+
+//function 4  letsrule1
+
+
+letsrule1:
+//prolog
+	pushl %ebp
+	movl %esp,%ebp
+//main part
+	movl 8(%ebp),%esi
+	movl $cop2,%edi
+	movl lengthS,%ecx
+loopf4:
+	movl %ecx, ptr
+	lodsb
+	cmpb $49,%al
+	jb f4end
+		cmpb $57,%al
+		ja f4end
+			addb $48 ,%al
+			stosb 
+			jmp f4endend
+			
+
+	f4end:
+		stosb
+		jmp f4endend
+	f4endend:
+	movl ptr,%ecx
+	loop loopf4
+//end of main part
+
+
+#printstring
+	pushl $strstr3
+	call printf
+	addl $4,%esp
+	
+	movl $cop2,%esi
+	movl lengthS,%ecx
+typing333:
+	movl %ecx, count
+	lodsb
+	movl %esi, ptr
+	movl %eax, a
+	pushl a
+	pushl $fmt_str
+	call printf
+	addl $8, %esp
+	movl ptr, %esi
+	movl count, %ecx
+	loop typing333
+
+	pushl $ennd
+	call printf
+	addl $4,%esp
+
+#end of printstring
+
+
+
+
+//epilog
+	movl %ebp,%esp
+	popl %ebp
+ret
+
+
+
 .globl main
 main:
 //prolog
@@ -174,8 +305,21 @@ loop typing
 	addl $8,%esp
 	
 //end of checking svoistvo
-
-
+	movl flag,%eax
+	cmpl $0,%eax
+	je  rule2
+		pushl $mytext
+		call letsrule1
+		addl $4,%esp
+		jmp eenndd
+//flag=0 rule 2
+rule2:
+	pushl	$mytext
+	call  letsrule2
+	addl $4,%esp
+	jmp eenndd
+	
+eenndd:
 //epilog
 	movl    $0,%eax
 	movl	%ebp, %esp
