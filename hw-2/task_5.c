cfb9af56b4e90d44b4162581dfa8774fd15f13c5
Volosheniuk Serhiy <Onspeeds@bigmir.net>
2014-05-17 20:28:03 +0400

    task 5 have been solved

---
+++ hw-2/task_5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+
+
+
+int sum1(int *a, int n);
+float sum2(float *f, int n);
+
+
+int main(){
+int i; int n = 7;
+int *a = (int *)malloc(n*sizeof(int));
+float *f = (float *)malloc(n*sizeof(float));
+	
+	
+	for (i=0; i<7; i++)
+		scanf("%d", &a[i]);
+	printf("%d\n", sum1(a, n));
+	
+	
+	
+	for (i=0; i<7; i++)
+		scanf("%f", &f[i]);
+	printf("%.2f\n",sum2(f, n));
+
+return 0;
+}
